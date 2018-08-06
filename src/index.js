07883c15fecdf2563878cf96f44a467609741f1e
i544c <leo@i544c.me>
2018-08-06 00:44:34 +0000

    Add connected-react-router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
 import rootReducer from "./reducers";
+// For connected-react-router
+import { createBrowserHistory } from "history";
+import { applyMiddleware, compose, createStore } from "redux";
+import { connectRouter, ConnectedRouter, routerMiddleware } from "connected-react-router";
+import { Route, Switch } from "react-router";
+// Local
 import "./index.css";
 import App from "./containers/AppContainer";
 import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(rootReducer);
+const history = createBrowserHistory();
+const initialState = {};
+const store = createStore(
+  connectRouter(history)(rootReducer),
+  initialState,
+  compose(
+    applyMiddleware(
+      routerMiddleware(history),
+    ),
+  ),
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <div>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route render={() => (<div>404</div>)} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
