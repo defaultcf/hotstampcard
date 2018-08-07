e2109cbfa7cd6cfbf9522f2b86c1d0a2b3aab972
i544c <leo@i544c.me>
2018-08-07 18:29:38 +0900

    Add function of date validate

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.validateStamp = functions.firestore
+  .document("stamps/{user}/data/{date}")
+  .onCreate((snap, context) => {
+    snap.ref.set({
+      time: new Date(),
+      valid: true,
+    });
+  });
